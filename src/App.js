c65065c5998986c160b6efeef54ad20bc6078fda
deboraharocho <deborah.arocho@gmail.com>
2025-04-04 23:17:27 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh', fontFamily: 'Arial' }}>
+      <h1>Deborah Arocho 5.4 GP Netfly</h1>
+      <p>Hi, this is my assignment submission for guided practice 5.4.</p>
+    </div>
+  );
+}
+
+export default App;
